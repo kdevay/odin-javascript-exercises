268e784afd66c2ba207557e800e7c275b8651844
Katherine de Vay <katdevay2@gmail.com>
2022-09-06 17:26:41 -0500

    Draft reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
 const reverseString = function() {
-
+    if (!string) {
+        return string;
+    }
+    string = string.split("");
+    string = string.reverse();
+    return string.join();
 };
 
 // Do not edit below this line
