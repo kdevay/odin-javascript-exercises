0ccfda51d7a1063cacd38a951918023a58a3099a
Katherine de Vay <katdevay2@gmail.com>
2022-09-06 16:59:47 -0500

    Draft repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(num, string) {
+    let finalString = "";
+    if (num < 1){
+        return "ERROR";
+    }
+    for (let i = 0; i < num; i++){
+        finalString += string;
+    }
+    return string;
 };
 
 // Do not edit below this line
