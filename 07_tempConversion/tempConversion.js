03710ab38d3914bce170d14fca51fdf5bd94d204
Katherine de Vay <katdevay2@gmail.com>
2022-09-07 12:23:13 -0500

    Fix typo in round method

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -3,7 +3,7 @@
 const ftoc = function(fTemp) {
   let cTemp = (fTemp - 32) / 1.8;
   // round
-  cTemp = (round(cTemp * 10))/10;
+  cTemp = (Math.round(cTemp * 10))/10;
   return cTemp;
 };
 
@@ -11,7 +11,7 @@ const ftoc = function(fTemp) {
 const ctof = function(cTemp) {
   let fTemp = (cTemp * 1.8) + 32;
   // round
-  fTemp = (round(fTemp * 10))/10;
+  fTemp = (Math.round(fTemp * 10))/10;
   return fTemp;
 };
 
