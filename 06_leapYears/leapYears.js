83369a14a1fbbfbb0ca1138e2ac9127417ac9cba
Katherine de Vay <katdevay2@gmail.com>
2022-09-07 11:56:50 -0500

    Draft leapYears function

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // years divisible by four 
+    if (!(year % 4)) {
+        console.log("hi");
+        } else if (!(year % 100) && !(year % 400)) { // years divisible by 400 and 100 
+        console.log("hi");
+        }
 };
 
 // Do not edit below this line
