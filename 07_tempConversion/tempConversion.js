1886df59b0457b980555d96f0ce9b31fb33452dd
Katherine de Vay <katdevay2@gmail.com>
2022-09-07 12:22:03 -0500

    Draft conversion functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,18 @@
-const ftoc = function() {
 
+// Convert fahrenheit to celcius
+const ftoc = function(fTemp) {
+  let cTemp = (fTemp - 32) / 1.8;
+  // round
+  cTemp = (round(cTemp * 10))/10;
+  return cTemp;
 };
 
-const ctof = function() {
-
+// Convert celsius to fahrenheit
+const ctof = function(cTemp) {
+  let fTemp = (cTemp * 1.8) + 32;
+  // round
+  fTemp = (round(fTemp * 10))/10;
+  return fTemp;
 };
 
 // Do not edit below this line
