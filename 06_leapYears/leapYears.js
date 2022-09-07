ed95d65a45133a04e4be33c32aed139cf92cade7
Katherine de Vay <katdevay2@gmail.com>
2022-09-07 12:01:58 -0500

    Add bool returns

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,10 +1,11 @@
 const leapYears = function(year) {
     // years divisible by four 
     if (!(year % 4)) {
-        console.log("hi");
-        } else if (!(year % 100) && !(year % 400)) { // years divisible by 400 and 100 
-        console.log("hi");
-        }
+        return true;
+    } else if (!(year % 100) && !(year % 400)) { // years divisible by 400 and 100 
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
